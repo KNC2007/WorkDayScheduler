94d3ea08e180b1dd10a617e0440ad7ffc342884b
KNC2007 <cnkalbach@gmail.com>
2023-10-25 21:38:41 -0400

    updated README and for loop for local storage get item

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # WorkDayScheduler
 Calendar application 
+
+## Description
+The calendar application allows users to save events for each hour of a typical working day (9am-5pm). The user builds a schedule and displays the current day and time at the top of the screen. There are time blocks for each hour. The time blocks are color-coded to indicate whether it is in the past, present, or future. Click the time block to enter an event or information. Then click the save button next to the time block. The text for the event is saved. When refreshed the events stay on the page. The user can remove the event and then save again and the event will stay deleted.
+
+## Screenshot
+The following image demonstrates the web application's appearence and functionality:
+![The screen displays a calendar for the current date and shows the current time. There are time blocks showing times between 9am and 5pm. The user can type in each box and press the save button next to the box.](./assets/screenshot%20work%20day%20scheduler.png)
+
+## Deployed Application
+Click here to visit the deployed Password Generator website: https://knc2007.github.io/WorkDayScheduler/
+
+## Resources
+- The following websites were used as reference for Javascript and jQuery: https://www.w3schools.com/ and https://developer.mozilla.org/en-US/ and https://htmlcheatsheet.com/jquery/ 
+- I had support from tutor Wongani "Jay" Jere. He helped with saving to local storage and getting from local storage.
--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,29 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
-
 var currentDate = $("#currentDay");
 var currentTime = parseInt(dayjs().format('H'));
-// var timeBlock = $(".time-block");
-var textAreaEl = $("textarea");
-calendarItems = [];
-// var timeTextEl = $("id");
-
-
-
 
 
 
 $(function () {
+  // display the time and date at the top of the calendar
   function displayCalendar() {
   var presentTime = dayjs().format('dddd, MMMM D, YYYY [at] hh:mm:ss a');
   currentDate.text(presentTime);
 }
 
 
+  // renders the calendar items on the page
+  for (var i = 9; i <= 17; i++) {
+    var textAreaEl = $(".description");
+    textAreaEl[i - 9].value = localStorage.getItem(i);
+  }
 
-$("#17 .description").val(localStorage.getItem(17))
-
-  
- $('.saveBtn').on("click", function(event) {
-  console.log($(this));
+  // saves the items on each time block of the calendar
+  $('.saveBtn').on("click", function(event) {
+      console.log($(this));
       var timeBlockToDo = $(this).parent().attr("id");
       console.log(timeBlockToDo);
       var calendarToDo = $(this).siblings("textarea").val();
       console.log(calendarToDo);
-      
       localStorage.setItem(timeBlockToDo, calendarToDo);
     })
 
@@ -40,6 +31,7 @@ $("#17 .description").val(localStorage.getItem(17))
 
   console.log(currentTime);
 
+// runs the color of the calendar hour blocks to show past times, future, and present
   function hourColor() {
     $(".description").each(function () {
       console.log(this);
@@ -54,31 +46,9 @@ $("#17 .description").val(localStorage.getItem(17))
       }});
     }
 
-  displayCalendar();
-  setInterval(displayCalendar, 1000);
-  hourColor();
-  saveCalendarItems();
-  renderCalendarItems();
+    displayCalendar();
+    setInterval(displayCalendar, 1000);
+    hourColor();
+    saveCalendarItems();
+    renderCalendarItems();
   })
-
-
-// event.preventDefault();
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
